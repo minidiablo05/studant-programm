6521824e296f42108913430a858a76e466347799
minidiablo05 <stepklim5@mail.ru>
2025-12-21 14:57:25 +0300

    personal try

--- javascript/final_pr/App.js
+++ javascript/final_pr/App.js
@@ -1,34 +1,61 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import SignUpScreen from './src/screens/SignUpScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import HomeScreen from './src/screens/HomeScreen';
+// Импортируем экраны
+import SignUpScreen from './screens/SignUpScreen';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
 
+// Создаем навигатор
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator vinitialRouteName="SignUp">
-          <Stack.Screen 
-            name="SignUp" 
-            component={SignUpScreen}
-          />
-          <Stack.Screen 
-            name="Login" 
-            component={LoginScreen}
-          />
-          <Stack.Screen 
-            name="Home" 
-            component={HomeScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="SignUp"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#bde5dd',
+          },
+          headerTintColor: '#1fab89',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 20,
+          },
+          headerTitleAlign: 'center',
+          cardStyle: {
+            backgroundColor: '#bde5dd',
+          },
+        }}
+      >
+        <Stack.Screen 
+          name="SignUp" 
+          component={SignUpScreen}
+          options={{
+            title: 'Регистрация',
+            headerShown: true,
+          }}
+        />
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen}
+          options={{
+            title: 'Вход в аккаунт',
+            headerShown: true,
+          }}
+        />
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{
+            title: 'Главная',
+            headerShown: true,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
