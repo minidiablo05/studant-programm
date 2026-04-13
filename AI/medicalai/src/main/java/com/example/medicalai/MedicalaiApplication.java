c67b3e9cd675c89c55f4749405711fda76845c1b
minidiablo05 <158159085+minidiablo05@users.noreply.github.com>
2026-04-13 14:17:08 +0300

    final2

---
+++ AI/medicalai/src/main/java/com/example/medicalai/MedicalaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicalai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalaiApplication.class, args);
+	}
+
+}
